5c95cecb0e5cc8184f89070d27cc7969504f87a4
Arie Sastra Hadiprawira <ariesastra@windowslive.com>
2021-09-02 20:52:04 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//DEPENDENCIES
+import React from 'react'
+
+// STYLE
+
+// COMPONENT
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your First Chat App</h1>
+    </div>
+  )
+}
+
+export default App

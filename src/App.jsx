b099cc09af4cd9512dd008f3ec97c3b734be5d97
Arie Sastra Hadiprawira <ariesastra@windowslive.com>
2021-09-02 21:00:41 +0700

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 //DEPENDENCIES
-import React from 'react'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
 // STYLE
 
 // COMPONENT
 
+// KEY for Stream Chat API
+const apiKey = '3f345uveczxk';
+const client = StreamChat.getInstance(apiKey);
+
 const App = () => {
   return (
-    <div>
-      <h1>Your First Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+
+        />
+        <ChannelContainer
+
+        />
+      </Chat>
     </div>
   )
 }
